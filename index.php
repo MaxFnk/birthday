0da6885667bc5c8be3d69f40a27740cb8a65992e
Max Franke <maximilian.franke@pwc.com>
2023-12-30 23:58:05 +0100

    update

--- index.php
+++ index.php
@@ -4,103 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Max wird 30</title>
-    <link rel="stylesheet" type="text/css" href="style.scss">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <script>
-        gsap.registerPlugin(Physics2DPlugin);
-
-document.querySelectorAll('.button').forEach(button => {
-
-    const bounding = button.getBoundingClientRect()
-
-    button.addEventListener('mousemove', e => {
-
-        let dy = (e.clientY - bounding.top - bounding.height / 2) / -1
-        let dx = (e.clientX - bounding.left - bounding.width / 2)  / 10
-
-        dy = dy > 10 ? 10 : (dy < -10 ? -10 : dy);
-        dx = dx > 4 ? 4 : (dx < -4 ? -4 : dx);
-
-        button.style.setProperty('--rx', dy);
-        button.style.setProperty('--ry', dx);
-
-    });
-
-    button.addEventListener('mouseleave', e => {
-
-        button.style.setProperty('--rx', 0)
-        button.style.setProperty('--ry', 0)
-
-    });
-
-    button.addEventListener('click', e => {
-        button.classList.add('success');
-        gsap.to(button, {
-            '--icon-x': -3,
-            '--icon-y': 3,
-            '--z-before': 0,
-            duration: .2,
-            onComplete() {
-                particles(button.querySelector('.emitter'), 100, -4, 6, -80, -50);
-                gsap.to(button, {
-                    '--icon-x': 0,
-                    '--icon-y': 0,
-                    '--z-before': -6,
-                    duration: 1,
-                    ease: 'elastic.out(1, .5)',
-                    onComplete() {
-                        button.classList.remove('success');
-                    }
-                });
-            }
-        });
-    });
-
-});
-
-function particles(parent, quantity, x, y, minAngle, maxAngle) {
-    let colors = [
-        '#FFFF04',
-        '#EA4C89',
-        '#892AB8',
-        '#4AF2FD',
-    ];
-    for(let i = quantity - 1; i >= 0; i--) {
-        let angle = gsap.utils.random(minAngle, maxAngle),
-            velocity = gsap.utils.random(70, 140),
-            dot = document.createElement('div');
-        dot.style.setProperty('--b', colors[Math.floor(gsap.utils.random(0, 4))]);
-        parent.appendChild(dot);
-        gsap.set(dot, {
-            opacity: 0,
-            x: x,
-            y: y,
-            scale: gsap.utils.random(.4, .7)
-        });
-        gsap.timeline({
-            onComplete() {
-                dot.remove();
-            }
-        }).to(dot, {
-            duration: .05,
-            opacity: 1
-        }, 0).to(dot, {
-            duration: 1.8,
-            rotationX: `-=${gsap.utils.random(720, 1440)}`,
-            rotationZ: `+=${gsap.utils.random(720, 1440)}`,
-            physics2D: {
-                angle: angle,
-                velocity: velocity,
-                gravity: 120
-            }
-        }, 0).to(dot, {
-            duration: 1,
-            opacity: 0
-        }, .8);
-    }
-}
-
-    </script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Inter:400,500,600,700&amp;display=swap'><link rel="stylesheet" href="./style.css">
 </head>
 <body>
     <div class="login-box">
@@ -140,5 +46,7 @@ function particles(parent, quantity, x, y, minAngle, maxAngle) {
 
     </form>
     </div>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.6/gsap.min.js'></script>
+<script src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/Physics2DPlugin3.min.js'></script><script  src="./script.js"></script>
 </body>
 </html>
