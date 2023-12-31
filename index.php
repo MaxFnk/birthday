4df43b123b107ac0edc3189d688ca9ab05b7bd8a
Max Franke <maximilian.franke@pwc.com>
2023-12-31 17:07:14 +0100

    favicon

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Max wird 30</title>
+    <link rel="icon" href="./favicon.png" type="image/png">
     <link rel="stylesheet" type="text/css" href="style.css" />
   </head>
   <body>
