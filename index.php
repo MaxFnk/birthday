565344f56d4e9773ecbc545e13452cc681ccefa0
Max Franke <maximilian.franke@pwc.com>
2023-12-31 15:18:14 +0100

    try it out

--- index.php
+++ index.php
@@ -1,41 +1,44 @@
 <!DOCTYPE html>
 <html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Max wird 30</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+  </head>
+  <body>
     <div class="login-box">
-        <h2>Max wird 30 - let's go!</h2>
-        <form action="submit.php" method="post">
-        
+      <h2>Max wird 30 - let's go!</h2>
+      <p style="color: white; text-align: center;">
+        27.01.2024
+        <br />
+        Lindenstraße 236, 40235 Düsseldorf
+        <br />
+        19 Uhr
+    </p>
+    <p style="color: white">
+    
+        Falls ihr Bock und Zeit habt, sagt gerne direkt zu.
+      </p>
+      <br />
+
+      <form action="submit.php" method="post">
         <div class="user-box">
-        <input type="text" id="vorname" name="vorname" required>
-        <label for="vorname">Vorname</label>
-        </div> 
+          <input type="text" id="vorname" name="vorname" required />
+          <label for="vorname">Vorname</label>
+        </div>
 
         <div class="user-box">
-        <input type="text" id="nachname" name="nachname" required>
-        <label for="nachname">Nachname</label>
-        </div> 
+          <input type="text" id="nachname" name="nachname" required />
+          <label for="nachname">Nachname</label>
+        </div>
 
         <div class="user-box">
-        <input type="text" id="email" name="email" required>
-        <label for="email">Email</label>
-        </div> 
-        
-        
-        <a href="#">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <input type="submit">
-      Bestätigen
-    </a>
-    </form>
+          <input type="text" id="email" name="email" required />
+          <label for="email">Email</label>
+        </div>
+        <button type="submit" id="submit-button">Bestätigen</button>
+      </form>
     </div>
-</body>
+  </body>
 </html>
--- submit.php
+++ submit.php
@@ -1,4 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Max wird 30</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+  </head>
+  <body>
+    <?php
 $serverName = "birthday.database.windows.net"; 
 $connectionOptions = array(
     "Database" => "birthday", // Ihr Datenbankname
@@ -28,8 +37,15 @@ if ($stmt === false) {
     die(print_r(sqlsrv_errors(), true));
 }
 
-echo "Teilnahme bestätigt!";
-
 sqlsrv_free_stmt($stmt);
 sqlsrv_close($conn);
 ?>
+
+    <div class="login-box">
+      <h2>Max wird 30 - du bist dabei</h2>
+      <p style="color: white; text-align: center;">
+        Ich freu mich auf Dich!
+    </p>
+    </div>
+  </body>
+</html>
